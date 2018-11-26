dcde1b89e4688747a00cc25ee8e4453eecd4b90a
OleHalvorE <oleh44@hotmail.com>
2018-11-26 15:13:50 +0100

    fikset buttons og h2 tag

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -1,7 +1,8 @@
 
-var textSize = "20";
-var headerSize = "40";
+var textSize = 20;
+var headerSize = 40;
 var currentColor = "white"
+var fontSizeCounter = 1;
 
 function changeFontSize(percent) {
     let x = document.body.querySelectorAll("*");
@@ -14,10 +15,20 @@ function changeFontSize(percent) {
         }
     });
 }
+
+function cchangefontSize() {
+    let button = document.getElementById("fontSize");
+    if( fontSizeCounter == 1 ) { changeFontSize(1.5); fontSizeCounter++; button.innerHTML = "Text Size: 150%"; }
+    else if( fontSizeCounter == 2 ) { changeFontSize(2.0); fontSizeCounter++; button.innerHTML = "Text Size: 200%"; }
+    else { changeFontSize(1.0); fontSizeCounter = 1; button.innerHTML = "Text Size: 100%"; }
+}
+
 function changeColor() {
+    let button = document.getElementById("contrast");
     if ( currentColor == "white") {
         currentColor = "black"
         document.body.style.background = "black";
+        button.innerHTML = "Contrast: White";
         let x = document.body.querySelectorAll("*");
         x.forEach(function(item) {
             if(item.id != "logo") {
@@ -39,6 +50,7 @@ function changeColor() {
     else {
         document.body.style.background = "white";
         currentColor = "white";
+        button.innerHTML = "Contrast: Black";
         let x = document.body.querySelectorAll("*");
         x.forEach(function(item) {
             if(item.id != "logo") {
