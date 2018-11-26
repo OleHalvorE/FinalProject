cdce59f483d6bc9e90b608f34fa3adff6a71d4d6
Rany Tarek Bouorm <rany-94@hotmail.com>
2018-11-26 11:01:02 +0100

    Video lagt inn
    Lagt inn video i index.html og controllern i videocontroller.js

---
+++ javascript/videocontroller.js
@@ -0,0 +1,70 @@
+/* Deklarasjon av variabler for knappene. */
+var btnAccessibilityOnOff = document.querySelector(".accessOnOff");
+var btnPlayPause = document.querySelector(".playpause");
+var btnStop = document.querySelector(".stop");
+var btnRestart = document.querySelector(".restart");
+var btnSubtitles = document.querySelector(".subtitles");
+var btnTranscript = document.querySelector(".transcript");
+
+/* Deklarasjon av variabler for DOM elementene */
+var vidAccControls = document.getElementById("videoAccessibilityControls");
+var videoPlayer = document.querySelector("video");
+var videocontrols = document.getElementById("videocontrols");
+var videoTranscript = document.getElementById("videoTranscript");
+
+/* Sikrer funksjonalitet ved deaktivert Javascript */
+videocontrols.style.display = "block";
+vidAccControls.style.display = "block";
+videoPlayer.controls = false;
+videoTranscript.style.display = "none";
+/*------------------------------------------------*/
+
+
+btnAccessibilityOnOff.onclick = function() {
+  if(videoPlayer.controls == true){
+    videoPlayer.controls = false;
+    btnAccessibilityOnOff.textContent = "Turn off accesible video controls"
+    videocontrols.style.display = "block";
+
+  } else {
+    videoPlayer.controls = true;
+    btnAccessibilityOnOff.textContent = "Turn on accesible video controls"
+    videocontrols.style.display = "none";
+  }
+}
+
+btnPlayPause.onclick = function() {
+  if(videoPlayer.paused) {
+    videoPlayer.play();
+    btnPlayPause.textContent = "Pause";
+  } else {
+    videoPlayer.pause();
+    btnPlayPause.textContent = "Play";
+  }
+}
+btnStop.onclick = function() {
+  videoPlayer.pause();
+  btnPlayPause.textContent = "Play";
+  videoPlayer.currentTime = 0;
+}
+btnRestart.onclick = function() {
+  videoPlayer.currentTime = 0;
+}
+btnSubtitles.onclick = function() {
+  if(videoPlayer.textTracks[0].mode == "disabled"){
+    videoPlayer.textTracks[0].mode = "showing";
+    btnSubtitles.textContent = "Turn off subtitles"
+  } else {
+    videoPlayer.textTracks[0].mode = "disabled";
+    btnSubtitles.textContent = "Turn on subtitles"
+  }
+}
+btnTranscript.onclick = function() {
+  if(videoTranscript.style.display === "none") {
+    videoTranscript.style.display = "block";
+    btnTranscript.textContent = "Hide video transcript"
+  } else {
+    videoTranscript.style.display = "none";
+    btnTranscript.textContent = "Show video transcript";
+  }
+}
