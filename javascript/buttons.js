831df5b046cd0617d6010910f0655cb941a82d6c
OleHalvorE <oleh44@hotmail.com>
2018-11-26 13:30:24 +0100

    ny logikk på contrast

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -1,6 +1,7 @@
 
 var textSize = "20";
 var headerSize = "40";
+var currentColor = "white"
 
 function changeFontSize(percent) {
     let x = document.body.querySelectorAll("*");
@@ -14,12 +15,13 @@ function changeFontSize(percent) {
     });
 }
 function changeColor() {
-    if ( document.body.style.background == "white none repeat scroll 0% 0%" || document.body.style.background  == "" ) {
+    if ( currentColor == "white") {
+        currentColor = "black"
         document.body.style.background = "black";
         let x = document.body.querySelectorAll("*");
         x.forEach(function(item) {
             if(item.id != "logo") {
-                if( item.className.split(" ")[0] == "playerbtn" ){
+                if( item.className.split(" ")[0] == "playerbtn" ) {
                     item.style.color = "rgb(24,23,23)";
                     item.style.background = "white";
                 }
@@ -36,6 +38,7 @@ function changeColor() {
     }
     else {
         document.body.style.background = "white";
+        currentColor = "white";
         let x = document.body.querySelectorAll("*");
         x.forEach(function(item) {
             if(item.id != "logo") {
@@ -50,3 +53,26 @@ function changeColor() {
         });
     }
 }
+
+var list_elements = document.getElementsByTagName("nav").firstChild;
+for(li in list_elements){
+    alert("dasdsa");
+    li.firstChild.addEventListener("click", function() {
+        alert("dasdsa");
+    });
+}
+window.onload = function() {
+    var list_elements = document.getElementsByTagName("nav").firstChild;
+    for(li in list_elements){
+        console.log(li.nodeValue);
+        li.firstChild.addEventListener("click", function() {
+            alert("dasdsa");
+        });
+    }
+}
+document.getElementsByTagName("NAV")[0].childNodes[0].addEventListener("mouseover", function(){
+    this.background = "green";
+});
+document.getElementsByTagName("NAV")[0].firstChild.addEventListener("mouseout", function(){
+    this.background = "green";
+}); 
